ccfd25038ff02c6ed5f2e5fa1d10172a49e82ab5
ahincho <ahincho@unsa.edu.pe>
2024-06-11 09:03:39 -0500

    Intro to Components and Routes

--- TraversyMedia/src/App.jsx
+++ TraversyMedia/src/App.jsx
@@ -1,22 +1,14 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter (
+  createRoutesFromElements (
+    <Route index element = {<HomePage/>}/>
+  )
+);
+
 const App = () => {
-  const name = 'John';
-  const x = 10;
-  const y = 20;
-  const names = ['Brad', 'Mary', 'Joe', 'Sara'];
-  const loggedIn = false;
-  const styles = { color: 'red', fontSize: '55px' };
-  return (
-    <>
-      <div className='text-5xl'>App</div>
-      <p style={styles}>Hello {name}</p>
-      <p>The sum of {x} and {y} is {x + y}</p>
-      <ul>
-        { names.map((name, index) => (
-          <li key={index}>{name}</li>
-        )) }
-      </ul>
-      { loggedIn ? <h1>Hello Member!</h1> : <h1>Hello Guest!</h1> }
-    </>
-  );
+  return <RouterProvider router={router}/>;
 };
+
 export default App;
---
+++ TraversyMedia/src/components/JobListings.jsx
@@ -0,0 +1,7 @@
+import jobs from '../jobs.json';
+
+const JobListings = () => {
+  console.log(jobs);
+  return;
+};
+export default JobListings;
